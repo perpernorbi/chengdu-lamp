bd8a5de53710e5e4ab5882a5e6359fafb5d65c90
KOSZÓ, Norbert <k.norbi@gmail.com>
2017-06-14 21:29:19 +0200

    I don't remember what I was doing

--- test-html.sh
+++ test-html.sh
@@ -2,7 +2,7 @@
 
 TEST_HTML_DIR="${TEST_HTML_DIR-"test-html"}"
 
-rm -rf "${TEST_HTML_DIR}"
+test -d "${TEST_HTML_DIR}" && ls "${TEST_HTML_DIR}" && rm -rf ${TEST_HTML_DIR}/*
 mkdir -p "${TEST_HTML_DIR}"
 
 TEST_HTML_DIR="$(readlink -e "${TEST_HTML_DIR}")"
@@ -14,17 +14,19 @@ for module in ${MODULES}; do
     pushd "${moduleHtmlDir}" > /dev/null
     find * -type d -exec mkdir -p "${TEST_HTML_DIR}/"{} \;
     for file in $(find * -type f); do
-        if echo ${file} | grep '\.tpl$' > /dev/null; then
-            targetFile="${TEST_HTML_DIR}/${file%%.*}.html"
-        else
+#        if echo ${file} | grep '\.tpl$' > /dev/null; then
+#            targetFile="${TEST_HTML_DIR}/${file%%.*}.html"
+#        else
             targetFile="${TEST_HTML_DIR}/${file}"
-        fi
+#        fi
         echo "${file} -> ${targetFile}"
         sedFile="${moduleHtmlTestDir}/templates/${file%%.*}.sed"
         if [ -f "${sedFile}" ]; then
             sed -f "${sedFile}" "${file}" > "${targetFile}"
+            sed -f "${sedFile}" "${file}" > "${targetFile%%.*}.html"
         else
             cp "${file}" "${targetFile}"
+            cp "${file}" "${targetFile%%.*}.html"
         fi
     done
     test -d "${moduleHtmlTestDir}/cgi" && ls ${moduleHtmlTestDir}/cgi/ && cp -r ${moduleHtmlTestDir}/cgi/* ${TEST_HTML_DIR}/
